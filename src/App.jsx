f9b1f99dcd0d0b14fd7161858da41c6c683af337
psddholariya03 <psddholariya03@outlook.com>
2024-02-14 09:23:17 +0530

    1- initial changes 2-creating src/ assets,components ,pages,utils  folders and files 3-creating src/assets/ images, wrappers folder and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
 // import React from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from "./pages";
 
-function App() {
-  return (
-    <div>job app</div>
-  )
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement:<Error />,
+    children:[
+      {
+        index:true,
+        element:<Landing />,
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+  },
+  // {
+  //   path: "/about",
+  //   element: (
+  //     <div>
+  //       <h2>about page</h2>
+  //     </div>
+  //   ),
+  // },
+]);
 
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 export default App;
